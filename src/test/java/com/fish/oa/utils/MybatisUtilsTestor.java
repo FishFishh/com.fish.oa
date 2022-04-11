d51ba6a17ed59f681a3408826867d29d0e0822c6
fish214057 <2140579318@qq.com>
2022-04-11 21:59:44 +0800

    Changes

---
+++ src/test/java/com/fish/oa/utils/MybatisUtilsTestor.java
@@ -0,0 +1,19 @@
+package com.fish.oa.utils;
+
+import org.junit.Test;
+
+public class MybatisUtilsTestor {
+    @Test
+    public void testcase1(){
+        String result = (String)MybatisUtils.executeQuery(sqlSession -> {
+            String out = (String)sqlSession.selectOne("test.sample");
+            return out;
+        });
+        System.out.println(result);
+    }
+    @Test
+    public void testcase2(){//就是上面的简化版（？
+        String result = (String) MybatisUtils.executeQuery(sqlSession -> sqlSession.selectOne("test.sample"));
+        System.out.println(result);
+    }
+}
