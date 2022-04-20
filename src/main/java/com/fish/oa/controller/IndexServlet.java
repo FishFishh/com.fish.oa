3b506a9f4ed4436bf8f2a937350864a8db513694
fish214057 <2140579318@qq.com>
2022-04-20 20:10:36 +0800

    Changes

--- src/main/java/com/fish/oa/controller/IndexServlet.java
+++ src/main/java/com/fish/oa/controller/IndexServlet.java
@@ -1,8 +1,35 @@
 package com.fish.oa.controller;
 
+
+import com.fish.oa.entity.Node;
+import com.fish.oa.entity.User;
+import com.fish.oa.service.UserService;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name="IndexServlet")
+@WebServlet(name = "IndexServlet",urlPatterns = "/index")
 public class IndexServlet extends HttpServlet {
+    private UserService userService = new UserService();
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        //得到当前登录用户对象
+        User user = (User)session.getAttribute("login_user");
+        //获取登录用户可用功能模块列表
+        List<Node> nodeList = userService.selectNodeByUserId(user.getUserId());
+        //放入请求属性
+        request.setAttribute("node_list" , nodeList);
+        //请求派发至ftl进行展现
+        request.getRequestDispatcher("/index.ftl").forward(request,response);
+    }
 }
