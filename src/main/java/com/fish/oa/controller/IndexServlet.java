d7f0333763d30401e47d060c1dca9cb552f533cf
fish214057 <2140579318@qq.com>
2022-04-20 19:48:17 +0800

    Changes

---
+++ src/main/java/com/fish/oa/controller/IndexServlet.java
@@ -0,0 +1,8 @@
+package com.fish.oa.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name="IndexServlet")
+public class IndexServlet extends HttpServlet {
+}
